71196b04b0ce0ab6ca8776aef6e4f20148a07f86
shayan fattahi <shayanfattahiii@yahoo.com>
2023-02-04 15:43:42 +0330

    working on Offered repo

---
+++ src/main/java/com/example/serviceprojectspring/repository/OfferedRepo.java
@@ -0,0 +1,10 @@
+package com.example.serviceprojectspring.repository;
+
+import com.example.serviceprojectspring.model.Offered;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface OfferedRepo extends JpaRepository<Offered, Long> {
+    Optional<OfferedRepo>readOfferedsByClientIdAndId_Order(Long id1 , Long id2);
+}
