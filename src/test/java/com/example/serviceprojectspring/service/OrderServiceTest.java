87ed1fcb62e9da95756b38e8f38bbf2b5c18e597
shayan fattahi <shayanfattahiii@yahoo.com>
2023-02-04 23:42:53 +0330

    create test class for order service

---
+++ src/test/java/com/example/serviceprojectspring/service/OrderServiceTest.java
@@ -0,0 +1,31 @@
+package com.example.serviceprojectspring.service;
+
+import junit.framework.TestCase;
+import lombok.AllArgsConstructor;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+@RunWith(JUnit4.class)
+@AllArgsConstructor
+public class OrderServiceTest extends TestCase {
+    @Test
+    public void createOrder() {
+    }
+
+    @Test
+    public void readLogInClientOrder() {
+    }
+
+    @Test
+    public void indefinitService() {
+    }
+
+    @Test
+    public void acceptedService() {
+    }
+
+    @Test
+    public void doneServices() {
+    }
+}
