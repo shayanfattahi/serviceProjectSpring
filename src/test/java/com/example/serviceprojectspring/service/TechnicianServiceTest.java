e432ef8ff1adfb678e5396735c0f07b4ebe15837
shayan fattahi <shayanfattahiii@yahoo.com>
2023-02-04 23:10:04 +0330

    create test for technician service

---
+++ src/test/java/com/example/serviceprojectspring/service/TechnicianServiceTest.java
@@ -0,0 +1,28 @@
+package com.example.serviceprojectspring.service;
+
+
+import com.example.serviceprojectspring.repository.usersRepo.TechnicianRepo;
+import junit.framework.TestCase;
+import lombok.AllArgsConstructor;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+@RunWith(JUnit4.class)
+@AllArgsConstructor
+public class TechnicianServiceTest extends TestCase {
+
+    private final TechnicianRepo technicianRepo;
+
+    @Test
+    public void emailExist() {
+    }
+
+    @Test
+    public void signUpTechnician() {
+    }
+
+    @Test
+    public void signInTechnician() {
+    }
+}
